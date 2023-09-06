382f1a04552359834d55edf762616b8a80442bac
Omar Allfuraydi <carboomar122@gmail.com>
2023-09-06 22:37:57 +0300

    LinerAlgebra Section Done.

--- src/main/Chapter2/DataManipulation.java
+++ src/main/Chapter2/DataManipulation.java
@@ -10,6 +10,7 @@ package main.Chapter2;
  *
  */
 import java.io.IOException;
+import static java.lang.System.exit;
 import java.util.List;
 import java.util.Arrays;
 import org.apache.mxnet.javaapi.Context;
@@ -19,30 +20,32 @@ import org.apache.mxnet.javaapi.Shape;
 import org.apache.mxnet.javaapi.random_normalParam;
 import org.apache.mxnet.javaapi.sumParam;
 import org.bytedeco.javacpp.Loader;
+import static java.lang.System.out;
+import static java.lang.System.setProperty;
 
 public class DataManipulation {
 
     public static void main(String[] args) throws IOException {
         Loader.load(org.bytedeco.mxnet.global.mxnet.class);
-        System.setProperty("org.bytedeco.openblas.load", "mkl");
+        setProperty("org.bytedeco.openblas.load", "mkl");
         Context context = Context.cpu();
 
-        System.out.println((char) 27 + "[32m" + "-Getting Started-" + (char) 27 + "[0m");
+        out.println((char) 27 + "[32m" + "-Getting Started-" + (char) 27 + "[0m");
         //args:(start value , end value , step ,repeat , context, and type) 
         NDArray array = NDArray.arange(0, 12, 1, 1, context, DType.Int32());
 
-        System.out.println("x: " + array.toString());
-        System.out.println("x Sizr: " + array.size());
-        System.out.println("x Shape: " + array.shape());
+        out.println("x: " + array.toString());
+        out.println("x Sizr: " + array.size());
+        out.println("x Shape: " + array.shape());
 
         array = array.reshape(new int[]{3, 4});
-        System.out.println("x ReShaped to 3,4: " + array.toString());
+        out.println("x ReShaped to 3,4: " + array.toString());
         //array = NDArray.arange(0, 12, 1, 1, context, DType.Int32());
-        System.out.println("x: " + array.toString());
+        out.println("x: " + array.toString());
 
         array = array.reshape(new int[]{-1, 4});
-        System.out.println("x ReShaped to using -1: " + array.toString());
-        System.out.println("---");
+        out.println("x ReShaped to using -1: " + array.toString());
+        out.println("---");
 
         random_normalParam randomParam = new random_normalParam();
         randomParam.setShape(new Shape(new int[]{3, 4}));
@@ -51,103 +54,103 @@ public class DataManipulation {
         NDArray[] randomArray = NDArray.random_normal(randomParam);
 
         for (NDArray nDArray : randomArray) {
-            System.out.println("Random Aray " + nDArray.toString());
+            out.println("Random Aray " + nDArray.toString());
         }
 
         NDArray construct_array = new NDArray(new float[]{2, 1, 4, 3, 1, 2, 3, 4, 4, 3, 2, 1}, new Shape(new int[]{3, 4,}), context);
-        System.out.println("constructed Aray " + construct_array.toString());
+        out.println("constructed Aray " + construct_array.toString());
 
-        System.out.println("---");
+        out.println("---");
         NDArray ones = NDArray.ones(context, List.of(2, 3, 4));
         NDArray zeros = NDArray.zeros(context, List.of(2, 3, 4));
-        System.out.println("Ones: " + Arrays.toString(ones.toArray()));
-        System.out.println("---");
-        System.out.println("Zeros: " + Arrays.toString(zeros.toArray()));
+        out.println("Ones: " + Arrays.toString(ones.toArray()));
+        out.println("---");
+        out.println("Zeros: " + Arrays.toString(zeros.toArray()));
 
-        System.out.println((char) 27 + "[32m" + "-Indexing and Slicing-" + (char) 27 + "[0m");
-        System.out.println("Array: " + Arrays.toString(array.toArray()));
+        out.println((char) 27 + "[32m" + "-Indexing and Slicing-" + (char) 27 + "[0m");
+        out.println("Array: " + Arrays.toString(array.toArray()));
 
         NDArray array_1to3Slice = array.slice(1, 3);
         NDArray array_Neg1 = array.slice(1, 3);
-        System.out.println("Array sliced 1_3" + Arrays.toString(array_1to3Slice.toArray()));
-        System.out.println("Array sliced -1" + Arrays.toString(array_Neg1.toArray()));
+        out.println("Array sliced 1_3" + Arrays.toString(array_1to3Slice.toArray()));
+        out.println("Array sliced -1" + Arrays.toString(array_Neg1.toArray()));
 
-        System.out.println("---");
-        System.out.println("---");
-        System.out.println((char) 27 + "[32m" + "Operations" + (char) 27 + "[0m");
+        out.println("---");
+        out.println("---");
+        out.println((char) 27 + "[32m" + "Operations" + (char) 27 + "[0m");
 
         NDArray expArray = null;
         expArray = array.copy();
 
         NDArray.exp(array, expArray);
-        System.err.println("Exp Array: " + Arrays.toString(expArray.toArray()));
+        out.println("Exp Array: " + Arrays.toString(expArray.toArray()));
 
         NDArray xArray = new NDArray(new float[]{1, 2, 4, 8}, new Shape(new int[]{4}), context);
 
         NDArray yArray = new NDArray(new float[]{2, 2, 2, 2}, new Shape(new int[]{4}), context);
 
-        System.err.println("X Array: " + Arrays.toString(xArray.toArray()));
-        System.err.println("Y Array: " + Arrays.toString(yArray.toArray()));
+        out.println("X Array: " + Arrays.toString(xArray.toArray()));
+        out.println("Y Array: " + Arrays.toString(yArray.toArray()));
 
-        System.out.println("X+Y: " + Arrays.toString(xArray.add(yArray).toArray()));
-        System.out.println("X-Y: " + Arrays.toString(xArray.subtract(yArray).toArray()));
-        System.out.println("X*Y: " + Arrays.toString(xArray.multiply(yArray).toArray()));
-        System.out.println("X/Y: " + Arrays.toString(xArray.div(yArray).toArray()));
-        System.out.println("X**Y: " + Arrays.toString(xArray.pow(yArray).toArray()));
+        out.println("X+Y: " + Arrays.toString(xArray.add(yArray).toArray()));
+        out.println("X-Y: " + Arrays.toString(xArray.subtract(yArray).toArray()));
+        out.println("X*Y: " + Arrays.toString(xArray.multiply(yArray).toArray()));
+        out.println("X/Y: " + Arrays.toString(xArray.div(yArray).toArray()));
+        out.println("X**Y: " + Arrays.toString(xArray.pow(yArray).toArray()));
 
-        System.out.println("-concatenate-");
+        out.println("-concatenate-");
 
         NDArray x = NDArray.arange(0, 12, 1, 1, context, DType.Float32());
         x = x.reshape(new int[]{3, 4});
 
         NDArray y = new NDArray(new float[]{2, 1, 4, 3, 1, 2, 3, 4, 4, 3, 2, 1}, new Shape(new int[]{3, 4,}), context);
-        System.out.println("X: " + x.toString());
-        System.out.println("Y: " + y.toString());
+        out.println("X: " + x.toString());
+        out.println("Y: " + y.toString());
 
         //axis Zero is x axis One is the Y axis
         //arg(the list of arrays , numbe of array in the list axis and output) 
         NDArray reuslt = NDArray.empty(context, new int[]{6, 4});
         NDArray.concat(new NDArray[]{x, y}, 2, 0, reuslt);
 
-        System.out.println("reuslt contact aixs 0: " + reuslt.toString());
+        out.println("reuslt contact aixs 0: " + reuslt.toString());
         reuslt = NDArray.empty(context, new int[]{3, 8});
         NDArray.concat(new NDArray[]{x, y}, 2, 1, reuslt);
-        System.out.println("reuslt contact aixs 1: " + reuslt.toString());
+        out.println("reuslt contact aixs 1: " + reuslt.toString());
 
-        System.out.println("X==y" + NDArray.equal(x, y).toString());
+        out.println("X==y" + NDArray.equal(x, y).toString());
 
         sumParam p = new sumParam(x);
 
-        System.out.println("X Sum: " + Arrays.toString(NDArray.sum(p)[0].toArray()));
+        out.println("X Sum: " + Arrays.toString(NDArray.sum(p)[0].toArray()));
 
-        System.out.println((char) 27 + "[32m" + "-1.4. Broadcasting-" + (char) 27 + "[0m");
+        out.println((char) 27 + "[32m" + "-1.4. Broadcasting-" + (char) 27 + "[0m");
 
         NDArray a = NDArray.arange(0, 3, 1, 1, context, DType.Int32());
         a = a.reshape(new int[]{3, 1});
-        System.out.println("a:" + a.toString());
+        out.println("a:" + a.toString());
         NDArray b = NDArray.arange(0, 2, 1, 1, context, DType.Int32());
         b = b.reshape(new int[]{1, 2});
-        System.out.println("b:" + b.toString());
-        System.out.println("a+b:" + NDArray.broadcast_add(a, b, null)[0].toString());
+        out.println("b:" + b.toString());
+        out.println("a+b:" + NDArray.broadcast_add(a, b, null)[0].toString());
 
         ///1.5. Saving Memory 
         //I dont think it is meant for us here for us here
-        System.err.println("---add and addinplace---");
+        out.println("---add and addinplace---");
 
         a = NDArray.arange(0, 3, 1, 1, context, DType.Int32());
         b = NDArray.arange(1, 4, 1, 1, context, DType.Int32());
 
-        System.out.println("a:" + a.toString());
+        out.println("a:" + a.toString());
         //Save Memoery by using addinplace
         NDArray c = a.add(b);
-        System.err.println("a != c" +Arrays.toString(NDArray.equal(a, c).toArray()));
+        out.println("a != c" +Arrays.toString(NDArray.equal(a, c).toArray()));
         a = NDArray.arange(0, 3, 1, 1, context, DType.Int32());
         b = NDArray.arange(1, 4, 1, 1, context, DType.Int32());
         c = a.addInplace(b);
-        System.err.println("a == c" +Arrays.toString(NDArray.equal(a, c).toArray()));
+        out.println("a == c" +Arrays.toString(NDArray.equal(a, c).toArray()));
         
         
-        System.exit(0);
+        exit(0);
 
     }
 
--- src/main/Chapter2/LinearAlgebra.java
+++ src/main/Chapter2/LinearAlgebra.java
@@ -15,113 +15,139 @@ import org.apache.mxnet.javaapi.dotParam;
 import org.apache.mxnet.javaapi.meanParam;
 import org.apache.mxnet.javaapi.sumParam;
 import org.apache.mxnet.javaapi.sum_axisParam;
+import static java.lang.System.*;
+import org.apache.mxnet.javaapi.normParam;
 
 /**
  *
  * @author omar
  */
 public class LinearAlgebra {
-
+    
     public static void main(String[] args) {
         //Scalaras 
         Context context = Context.cpu();
         NDArray x = new NDArray(new float[]{3}, new Shape(new int[]{1}), context);
         NDArray y = new NDArray(new float[]{2}, new Shape(new int[]{1}), context);
-
-        System.out.println("X+Y: " + Arrays.toString(x.add(y).toArray()));
-        System.out.println("X*Y: " + Arrays.toString(x.multiply(y).toArray()));
-        System.out.println("X/Y: " + Arrays.toString(x.div(y).toArray()));
-        System.out.println("X**Y: " + Arrays.toString(x.pow(y).toArray()));
+        
+        out.println("X+Y: " + Arrays.toString(x.add(y).toArray()));
+        out.println("X*Y: " + Arrays.toString(x.multiply(y).toArray()));
+        out.println("X/Y: " + Arrays.toString(x.div(y).toArray()));
+        out.println("X**Y: " + Arrays.toString(x.pow(y).toArray()));
 
         //Vectors 
         NDArray vector = NDArray.arange(0, 3, 1, 1, context, DType.Float64());
-        System.out.println("Vectors:" + Arrays.toString(vector.toArray()));
-        System.out.println("Vector 2nd Element" + Arrays.toString(vector.slice(2).toArray()));
-        System.out.println("Vextor Length:" + vector.size());
-        System.out.println("Vector Shape:" + Arrays.toString(vector.shape().toArray()));
+        out.println("Vectors:" + Arrays.toString(vector.toArray()));
+        out.println("Vector 2nd Element" + Arrays.toString(vector.slice(2).toArray()));
+        out.println("Vextor Length:" + vector.size());
+        out.println("Vector Shape:" + Arrays.toString(vector.shape().toArray()));
 
         //Matrices
         NDArray matrixThreeByTwo = NDArray.arange(0, 6, 1, 1, context, DType.Int32());
         matrixThreeByTwo = matrixThreeByTwo.reshape(new int[]{3, 2});
-
-        System.out.println("Matrix:" + matrixThreeByTwo.toString());
-        System.out.println("Matrix:" + matrixThreeByTwo.T().toString());
+        
+        out.println("Matrix:" + matrixThreeByTwo.toString());
+        out.println("Matrix:" + matrixThreeByTwo.T().toString());
         //Matrix are  equal to their T // A==A.T if they symmetric 
         NDArray symmetricMatrix = new NDArray(new float[]{1, 2, 3, 2, 0, 4, 3, 4, 5}, new Shape(new int[]{3, 3}), context);
-        System.out.println("A==A.T" + NDArray.equal(symmetricMatrix, symmetricMatrix.T()).toString());
+        out.println("A==A.T" + NDArray.equal(symmetricMatrix, symmetricMatrix.T()).toString());
 
         //Tnsors 
         NDArray tensor = NDArray.arange(0, 24, 1, 1, context, DType.Float64());
         tensor = tensor.reshape(new int[]{2, 3, 4});
-        System.out.println("Matrix:" + tensor.toString());
-
-        System.out.println("===");
+        out.println("Matrix:" + tensor.toString());
+        
+        out.println("===");
         //Basic Properties of Tensor Arithmetic
         NDArray a = NDArray.arange(0, 6, 1, 1, context, DType.Float64());
         a = a.reshape(new int[]{2, 3});
-        System.out.println("Matrix A:" + a.toString());
+        out.println("Matrix A:" + a.toString());
         NDArray b = null;
         b = a.copy();
-        System.out.println("Matrix B:" + b.toString());
-        System.out.println("X+Y: " + a.add(b).toString());
-        System.out.println("X*Y: " + a.multiply(b).toString());
-        System.out.println("===");
+        out.println("Matrix B:" + b.toString());
+        out.println("X+Y: " + a.add(b).toString());
+        out.println("X*Y: " + a.multiply(b).toString());
+        out.println("===");
         //Tensor * scalar ;
-        System.out.println("2+Tensor: " + tensor.add(2).toString());
-        System.out.println("2*Tensor: " + tensor.multiply(2).shape().toString());
-        System.out.println("===");
+        out.println("2+Tensor: " + tensor.add(2).toString());
+        out.println("2*Tensor: " + tensor.multiply(2).shape().toString());
+        out.println("===");
 
         //Reduction
         sumParam sumParam = new sumParam(vector);
-        System.out.println("Vectors:" + Arrays.toString(vector.toArray()));
-        System.out.println("vertor Sum: " + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
-        System.out.println("===");
+        out.println("Vectors:" + Arrays.toString(vector.toArray()));
+        out.println("vertor Sum: " + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
+        out.println("===");
         sumParam = new sumParam(a);
-        System.out.println("Matrix Shape:" + Arrays.toString(a.shape().toArray()));
-        System.out.println("Matrix Sum: " + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
+        out.println("Matrix Shape:" + Arrays.toString(a.shape().toArray()));
+        out.println("Matrix Sum: " + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
         sum_axisParam p1 = new sum_axisParam(a);
-        System.out.println("===");
+        out.println("===");
         p1.setAxis(new Shape(List.of(0)));
-        //System.out.println("Shape " + Arrays.toString(p1.getAxis().shape().toArray()));
-        System.out.println("Matrix Sum axis zero Shape: " + Arrays.toString(NDArray.sum_axis(p1)[0].shape().toArray()));
+        //out.println("Shape " + Arrays.toString(p1.getAxis().shape().toArray()));
+        out.println("Matrix Sum axis zero Shape: " + Arrays.toString(NDArray.sum_axis(p1)[0].shape().toArray()));
         p1.setAxis(new Shape(List.of(1)));
-        System.out.println("Matrix Sum axis one Shape: " + Arrays.toString(NDArray.sum_axis(p1)[0].shape().toArray()));
+        out.println("Matrix Sum axis one Shape: " + Arrays.toString(NDArray.sum_axis(p1)[0].shape().toArray()));
         p1.setAxis(new Shape(List.of(0, 1)));
-        System.out.println("Matrix Sum axis one and Zero == array.Sum : " + NDArray.equal(NDArray.sum_axis(p1)[0], a.sum(sumParam)[0]).toString());
-        System.out.println("===");
+        out.println("Matrix Sum axis one and Zero == array.Sum : " + NDArray.equal(NDArray.sum_axis(p1)[0], a.sum(sumParam)[0]).toString());
+        out.println("===");
         meanParam meanParam = new meanParam(a);
         //Matrix Mean == Matrix Sum/Size
-        System.out.println("Matrix Mean" + NDArray.mean(meanParam)[0].toString());
-        System.out.println("Matrixc Sum/size" + NDArray.sum(sumParam)[0].div(a.size()).toString());
+        out.println("Matrix Mean" + NDArray.mean(meanParam)[0].toString());
+        out.println("Matrixc Sum/size" + NDArray.sum(sumParam)[0].div(a.size()).toString());
         //Matrix Mean == Matrix Sum/Size // Using 0 Axis
         meanParam.setAxis(new Shape(List.of(0)));
         p1.setAxis(new Shape(List.of(0)));
-        System.out.println("===");
-        System.out.println("Matrix Mean axis 0:" + NDArray.mean(meanParam)[0].toString());
-        System.out.println("Matrixc Sum/size axis 0:" + NDArray.sum_axis(p1)[0].div(a.shape().toArray()[0]).toString());
-        System.out.println("===");
+        out.println("===");
+        out.println("Matrix Mean axis 0:" + NDArray.mean(meanParam)[0].toString());
+        out.println("Matrixc Sum/size axis 0:" + NDArray.sum_axis(p1)[0].div(a.shape().toArray()[0]).toString());
+        out.println("===");
         //Non-Reduction Sum 
         p1.setAxis(new Shape(new int[]{1}));
         p1.setKeepdims(true);
-        System.out.println("Sum a with keepdims True allong axis 0: " + NDArray.sum_axis(p1)[0].toString());
-        System.out.println("Sum a / Sum Sof A axis 1: //Also known as boardcast" + NDArray.broadcast_div(a, NDArray.sum_axis(p1)[0], null)[0].toString());
+        out.println("Sum a with keepdims True allong axis 0: " + NDArray.sum_axis(p1)[0].toString());
+        out.println("Sum a / Sum Sof A axis 1: //Also known as boardcast" + NDArray.broadcast_div(a, NDArray.sum_axis(p1)[0], null)[0].toString());
         cumsumParam c = new cumsumParam(a);
         c.setAxis(0);
-        System.out.println("Cumulative sum: " + NDArray.cumsum(c)[0].toString());
-        System.out.println("===");
+        out.println("Cumulative sum: " + NDArray.cumsum(c)[0].toString());
+        out.println("===");
+
         //Dot Products
-        y = NDArray.ones(new Shape( List.of(3)),context,DType.Float64());
+        y = NDArray.ones(new Shape(List.of(3)), context, DType.Float64());
         x = NDArray.arange(0, 3, 1, 1, context, DType.Float64());
-        System.out.println("Y: " + y.toString());
-        System.out.println("X: " + x.toString());
+        out.println("Y: " + y.toString());
+        out.println("X: " + x.toString());
         dotParam dtprm = new dotParam(y, x);
-        System.out.println("y . x : " + NDArray.dot(dtprm)[0].toString());
+        out.println("y . x : " + NDArray.dot(dtprm)[0].toString());
         sumParam = new sumParam(y.multiply(x));
-        System.out.println("Sum of x * Y: "+NDArray.sum(sumParam)[0].toString());
+        out.println("Sum of x * y: " + NDArray.sum(sumParam)[0].toString());
+        out.println("===");
+
+        //Matrix–Vector Products
+        dtprm = new dotParam(a, x);
+        out.println("A Shapre" + Arrays.toString(a.shape().toArray()) + " X Shapre:" + Arrays.toString(x.shape().toArray()));
+        out.println("A dot x : " + NDArray.dot(dtprm)[0].toString());
+        out.println("===");
+
+        //Norms
+        NDArray u = new NDArray(new float[]{3, -4}, new Shape(new int[]{2}), context);
+        normParam nrmprm = new normParam(u);
+        out.println("Norm of u = {3,-4}" + Arrays.toString(NDArray.norm(nrmprm)[0].toArray()));
+        sumParam = new sumParam(u);
+        sumParam.setExclude(false);
+        out.println("Norm2 of u = {3,-4}" + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
+        NDArray abs_u = NDArray.abs(u, null)[0];
+        out.println("abs of u : " + abs_u.toString());
+        sumParam = new sumParam(abs_u);
+        out.println("Norm1 of u = {3,-4}" + Arrays.toString(NDArray.sum(sumParam)[0].toArray()));
         
-         //Matrix–Vector Products
+        NDArray f = NDArray.ones(context, List.of(4, 9));
+        out.println("ones f : " + f.shape().toString());
+        nrmprm = new normParam(f);
+        out.println("Norm of f" + Arrays.toString(NDArray.norm(nrmprm)[0].toArray()));
         
+        exit(0);
         
     }
-
+    
 }
